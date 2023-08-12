558c383ff7be59599bfa32152d18ca7d92c15e3f
Ligia Alves <ligia.stalves@gmail.com>
2023-08-12 10:09:57 -0300

    created the classes vaga and candidato, Spring created tables in the database wi+-th hibernate

---
+++ src/main/java/com/br/AppRH/AppRH/models/Vaga.java
@@ -0,0 +1,79 @@
+package com.br.AppRH.AppRH.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+public class Vaga implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long codigo;
+
+    @NotEmpty
+    private String nome;
+
+    @NotEmpty
+    private String descricao;
+
+    @NotEmpty
+    private String data;
+
+    @NotEmpty
+    private String salario;
+
+    @OneToMany(mappedBy = "vaga", cascade = CascadeType.REMOVE)
+    private List<Candidatos> candidatos;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getSalario() {
+        return salario;
+    }
+
+    public void setSalario(String salario) {
+        this.salario = salario;
+    }
+
+    public List<Candidatos> getCandidatos() {
+        return candidatos;
+    }
+
+    public void setCandidatos(List<Candidatos> candidatos) {
+        this.candidatos = candidatos;
+    }
+}
