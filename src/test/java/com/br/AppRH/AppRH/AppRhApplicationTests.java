d45d481c9c77c22855055367ad8f3750cabf5548
Ligia Alves <ligia.stalves@gmail.com>
2023-08-12 08:51:19 -0300

    creating an application and database connection class

---
+++ src/test/java/com/br/AppRH/AppRH/AppRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.AppRH.AppRH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
